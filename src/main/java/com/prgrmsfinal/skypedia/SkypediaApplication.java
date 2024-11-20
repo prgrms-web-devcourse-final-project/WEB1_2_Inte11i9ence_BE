0b666198fcc3632ea1d33c2e30859c1eb9a855eb
jungyoeal <74736179+jungyoeal@users.noreply.github.com>
2024-11-20 11:43:53 +0900

    FIX : "파일 위치 변경"

---
+++ src/main/java/com/prgrmsfinal/skypedia/SkypediaApplication.java
@@ -0,0 +1,13 @@
+package com.prgrmsfinal.skypedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkypediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkypediaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prgrmsfinal/skypedia/SkypediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgrmsfinal.skypedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkypediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
