0f7538f4494af8bab4e7552e27861bf69187e7d0
ghtndl <leesooho0406@gmail.com>
2024-11-22 14:20:27 +0900

    Comment: 멤버 엔티티 주석 추가

--- src/main/java/com/prgrmsfinal/skypedia/member/entity/Member.java
+++ src/main/java/com/prgrmsfinal/skypedia/member/entity/Member.java
@@ -19,30 +19,31 @@ import java.time.LocalDateTime;
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long id;                //자동생성 고유식별자
 
-    private String oauthId;
+    private String oauthId;         //구글, 네이버에서 제공하는 고유식별자
 
     @Column(nullable = false)
-    private String name;
+    private String name;            //구글, 네이버에서 제공하는 본명
 
-    private String email;
+    private String email;           //구글, 네이버에서 제공하는 이메일
 
     @Column(unique = true)
-    private String username;
+    private String username;        //최초회원가입시, 제공되는 랜덤 닉네임
 
-    private String role;
-    private String profileImage;
+    private String role;            //유저 권한
+
+    private String profileImage;    //사용자 프로필사진
 
     @CreatedDate
-    private LocalDateTime createdAt;
+    private LocalDateTime createdAt;//회원가입 날짜
     @LastModifiedDate
-    private LocalDateTime updatedAt;
+    private LocalDateTime updatedAt;//업데이트 날짜
 
-    private boolean withdrawn;
+    private boolean withdrawn;      //탈퇴여부
 
     @LastModifiedDate
-    private LocalDateTime withdrawnAt;
+    private LocalDateTime withdrawnAt;//탈퇴 날짜
 
 
 }
