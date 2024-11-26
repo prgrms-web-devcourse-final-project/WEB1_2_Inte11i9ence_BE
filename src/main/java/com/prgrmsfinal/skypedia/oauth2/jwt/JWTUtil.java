435eba10635983fc8f447658abeac42b946e93dc
ghtndl <leesooho0406@gmail.com>
2024-11-26 16:40:45 +0900

    Refactor: JwtParserBuilder 메서드화

--- src/main/java/com/prgrmsfinal/skypedia/oauth2/jwt/JWTUtil.java
+++ src/main/java/com/prgrmsfinal/skypedia/oauth2/jwt/JWTUtil.java
@@ -1,6 +1,7 @@
 package com.prgrmsfinal.skypedia.oauth2.jwt;
 
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -16,24 +17,30 @@ public class JWTUtil {  //토큰을 제작하는 클래스
     private SecretKey secretKey;
 
     public JWTUtil(@Value("${spring.jwt.secret}")String secret) {
-
-
         secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    private Claims parseClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 
     public String getOauthId(String token) {
 
-        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("oauthId", String.class);
+        return parseClaims(token).get("oauthId", String.class);
     }
 
     public String getRole(String token) {
 
-        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("role", String.class);
+        return parseClaims(token).get("role", String.class);
     }
 
     public Boolean isExpired(String token) {
 
-        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+        return parseClaims(token).getExpiration().before(new Date());
     }
 
     public String createJwt(String oauthId, String role, Long expiredMs) {
