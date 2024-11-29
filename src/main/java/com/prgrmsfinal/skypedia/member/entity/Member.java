42dc32040d6c81cfc5e006cc1250f57c8b6638d8
ghtndl <leesooho0406@gmail.com>
2024-11-29 11:06:12 +0900

    Refactor: nullable 설정

--- src/main/java/com/prgrmsfinal/skypedia/member/entity/Member.java
+++ src/main/java/com/prgrmsfinal/skypedia/member/entity/Member.java
@@ -23,20 +23,25 @@ public class Member extends BaseTime {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;                //자동생성 고유식별자
 
+    @Column(nullable = false)
     private String oauthId;         //구글, 네이버에서 제공하는 고유식별자
 
     @Column(nullable = false)
     private String name;            //구글, 네이버에서 제공하는 본명
 
+    @Column(nullable = false)
     private String email;           //구글, 네이버에서 제공하는 이메일
 
     @Column(unique = true)
     private String username;        //최초회원가입시, 제공되는 랜덤 닉네임
 
+    @Column(nullable = false)
     private String role;            //유저 권한
 
+    @Column(nullable = false)
     private String profileImage;    //사용자 프로필사진
 
+    @Column(nullable = false)
     private boolean withdrawn = Boolean.FALSE; // 탈퇴 여부
 
     private LocalDateTime withdrawnAt;//탈퇴 날짜
