dc50fd6c4622602572d64c303d517b06ff75c200
jungyoeal <74736179+jungyoeal@users.noreply.github.com>
2024-12-03 19:38:28 +0900

    Update: SecurityConfig.java
    모든 url 허용

--- src/main/java/com/prgrmsfinal/skypedia/oauth2/config/SecurityConfig.java
+++ src/main/java/com/prgrmsfinal/skypedia/oauth2/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
 
 					CorsConfiguration configuration = new CorsConfiguration();
 
-					configuration.setAllowedOrigins(Collections.singletonList(frontendUrl));
+					configuration.setAllowedOrigins(Collections.singletonList("*"));
 					configuration.setAllowedMethods(Collections.singletonList("*"));
 					configuration.setAllowCredentials(true);
 					configuration.setAllowedHeaders(Collections.singletonList("*"));
