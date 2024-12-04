38e971e8f6db2c21d35693c7804d07aa8e956bc4
SUHO LEE <117787238+ghtndl@users.noreply.github.com>
2024-12-04 12:02:55 +0900

    Update SecurityConfig.java

--- src/main/java/com/prgrmsfinal/skypedia/oauth2/config/SecurityConfig.java
+++ src/main/java/com/prgrmsfinal/skypedia/oauth2/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
 
 					CorsConfiguration configuration = new CorsConfiguration();
 
-					configuration.setAllowedOrigins(Collections.singletonList("*"));
+					configuration.setAllowedOrigins(Collections.singletonList(frontendUrl));
 					configuration.setAllowedMethods(Collections.singletonList("*"));
 					configuration.setAllowCredentials(true);
 					configuration.setAllowedHeaders(Collections.singletonList("*"));
