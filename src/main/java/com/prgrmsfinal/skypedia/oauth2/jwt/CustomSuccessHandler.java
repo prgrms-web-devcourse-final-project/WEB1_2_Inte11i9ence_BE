5384c1f072808e6c400e4e4aba788cffa4d05d85
ghtndl <leesooho0406@gmail.com>
2024-12-05 14:24:02 +0900

    Fix: localhost test 성공 커밋

--- src/main/java/com/prgrmsfinal/skypedia/oauth2/jwt/CustomSuccessHandler.java
+++ src/main/java/com/prgrmsfinal/skypedia/oauth2/jwt/CustomSuccessHandler.java
@@ -59,8 +59,8 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
         Cookie cookie = new Cookie(key, value);
         cookie.setMaxAge(60*60*60);
-//        cookie.setAttribute("SameSite","None");
-//        cookie.setSecure(true);
+        cookie.setAttribute("SameSite","None");
+        cookie.setSecure(true);
         cookie.setPath("/");
         cookie.setHttpOnly(true);
 
