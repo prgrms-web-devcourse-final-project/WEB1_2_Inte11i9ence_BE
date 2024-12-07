5505b29ea99c75ce5609262d6fd8e1c61bd52032
juwon-code <juwoncode@gmail.com>
2024-12-07 22:03:01 +0900

    Feat: 게시글 카테고리 API 구현
    게시글 카테고리 생성, 단일 조회, 목록 조회 API를 구현했습니다.

--- src/main/java/com/prgrmsfinal/skypedia/post/service/PostCategoryService.java
+++ src/main/java/com/prgrmsfinal/skypedia/post/service/PostCategoryService.java
@@ -1,10 +1,21 @@
 package com.prgrmsfinal.skypedia.post.service;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.security.core.Authentication;
+
+import com.prgrmsfinal.skypedia.post.dto.PostCategoryRequestDTO;
+import com.prgrmsfinal.skypedia.post.dto.PostCategoryResponseDTO;
 import com.prgrmsfinal.skypedia.post.entity.PostCategory;
 
 public interface PostCategoryService {
+	PostCategoryResponseDTO.Read read(String name);
+
+	List<PostCategoryResponseDTO.Read> readAll();
+
+	void create(Authentication authentication, PostCategoryRequestDTO.Create request);
+
 	Optional<PostCategory> getByName(String name);
 
 	boolean existsByName(String name);
