75912916e65524d1e303078ac00bb1d372b9fd74
juwon-code <juwoncode@gmail.com>
2024-12-08 13:06:39 +0900

    Fix: 누락된 컨트롤러 애너테이션 추가
    요청 DTO 검증에서 발생할 수 있는 예외들을 처리하고, 반환 메시지도 DTO로 규격화했습니다.
    
    Related to: #75

--- src/main/java/com/prgrmsfinal/skypedia/post/controller/PostCategoryController.java
+++ src/main/java/com/prgrmsfinal/skypedia/post/controller/PostCategoryController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +18,13 @@ import com.prgrmsfinal.skypedia.post.dto.PostCategoryResponseDTO;
 import com.prgrmsfinal.skypedia.post.service.PostCategoryService;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/post-category")
+@Validated
 @Tag(name = "게시글 카테고리 API 컨트롤러", description = "게시글 카테고리와 관련된 REST API를 제공하는 컨트롤러입니다.")
 public class PostCategoryController {
 	private final PostCategoryService postCategoryService;
@@ -38,7 +42,7 @@ public class PostCategoryController {
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void create(Authentication authentication, PostCategoryRequestDTO.Create request) {
+	public void create(Authentication authentication, @Valid @RequestBody PostCategoryRequestDTO.Create request) {
 		postCategoryService.create(authentication, request);
 	}
 }
