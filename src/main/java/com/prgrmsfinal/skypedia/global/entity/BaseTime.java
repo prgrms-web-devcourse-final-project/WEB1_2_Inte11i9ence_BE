2ee30b404f8ddf027a9116563c260db4ca1d8cb8
juwon-code <juwoncode@gmail.com>
2024-12-08 15:54:11 +0900

    Fix: 시간 관련 500 에러 픽스
    시간 관련 필드에 삽입과 수정을 금지했습니다.
    
    Related to: #75

--- src/main/java/com/prgrmsfinal/skypedia/global/entity/BaseTime.java
+++ src/main/java/com/prgrmsfinal/skypedia/global/entity/BaseTime.java
@@ -2,13 +2,16 @@ package com.prgrmsfinal.skypedia.global.entity;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 
 @Getter
 @MappedSuperclass
 public class BaseTime {
+	@Column(insertable = false, updatable = false)
 	private LocalDateTime createdAt;
 
+	@Column(insertable = false, updatable = false)
 	private LocalDateTime updatedAt;
 }
