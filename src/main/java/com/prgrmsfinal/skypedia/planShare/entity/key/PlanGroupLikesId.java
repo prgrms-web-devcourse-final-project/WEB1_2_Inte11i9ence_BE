7819d4adac3dc4a446c00069f9eeed600843776b
juwon-code <juwoncode@gmail.com>
2024-12-10 16:24:33 +0900

    Fix: 복합키 관련 경고 및 DB 방언 설정 제거
    - 각 도메인의 복합키에 `@EqualsAndHashCode`를 추가.
    - 더이상 사용되지 않는 MySQL Dialect 설정 제거.
    
    Related to: #94

--- src/main/java/com/prgrmsfinal/skypedia/planShare/entity/key/PlanGroupLikesId.java
+++ src/main/java/com/prgrmsfinal/skypedia/planShare/entity/key/PlanGroupLikesId.java
@@ -3,8 +3,10 @@ package com.prgrmsfinal.skypedia.planShare.entity.key;
 import java.io.Serializable;
 
 import jakarta.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
 
 @Embeddable
+@EqualsAndHashCode
 public class PlanGroupLikesId implements Serializable {
 	private Long planGroupId;
 
--- src/main/java/com/prgrmsfinal/skypedia/post/entity/key/PostLikesId.java
+++ src/main/java/com/prgrmsfinal/skypedia/post/entity/key/PostLikesId.java
@@ -14,6 +14,7 @@ import lombok.Setter;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 public class PostLikesId implements Serializable {
 	private Long postId;
 
